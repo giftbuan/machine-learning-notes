ee776570f0f4bc3f2d5a3a3801baa7bf6f5819b6
Gift Buan <giftbuan0818@gmail.com>
2026-01-19 10:18:22 +0800

    added more notes

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+# # This is a sample Python script.
+#
+# # Press ⌃F5 to execute it or replace it with your code.
+# # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from matplotlin import pyplot as plt
+from itertools import count #add the counter so we can move
+from matplotlin.animation import FuncAnimation #will animate the graph
+import pandas as pd
+import random
 
+plt.style.use('fivethirtyeight')
+x_vals = []
+y_vals = []
+index = count()
+def animate(i):
+    x_vals.append(next(index))
+    y_vals.append(random.randint(0,5))
+    plt.cla()
+    plt.plot(x_vals, y_vals)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    ani = FuncAnimation(plt.gcf(), animate, interba)
